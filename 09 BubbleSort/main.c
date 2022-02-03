e72eb72a9c28aa4bc2e310be39f42a6e499417e3
Luis Carlos <luis.carlos@jornadatecnologia.com>
2022-02-02 23:43:51 -0300

    Feat: BubbleSort.

--- 09 BubbleSort/main.c
+++ 09 BubbleSort/main.c
@@ -20,8 +20,30 @@ int main()
         printf("%d\n", numero);
     }
 
+    alteracoes = 1;
+    int temporario = 0;
+
     while (alteracoes > 0)
     {
+        alteracoes = 0;
+        for (i = 0; i < 4; i++)
+        {
+            if (vetor[i] > vetor[i + 1])
+            {
+                numero = vetor[i];
+                temporario = vetor[i];
+                vetor[i] = vetor[i + 1];
+                vetor[i + 1] = temporario;
+                alteracoes = alteracoes + 1;
+            }
+        }
+    }
+
+    printf("\n Vetor Ordenado:  \n\n");
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d \n", vetor[i]);
     }
 
     return 0;
